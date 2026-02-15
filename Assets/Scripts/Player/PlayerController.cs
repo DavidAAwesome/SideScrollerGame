2188abd68d52e8f63e2ea6dae39943196e4b2247
David Aduku <130676739+DavidAAwesome@users.noreply.github.com>
2026-02-15 13:18:06 -0500

    Player Movement

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,64 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("PlayerStats")]
+    public float speed;
+    
+    [Header("Jump")]
+    public float jumpVelocity;
+    public float fallGravityMultiplier = 3f;
+    public float riseGravityMultiplier = 2f;
+    
+    [Header("GroundCheck")]
+    public Transform groundCheck;
+    public float groundRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool grounded;
+    private float baseGravity;
+
+    void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        baseGravity = rb.gravityScale;
+    }
+    
+    void Update()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
+    }
+
+    private void FixedUpdate()
+    {
+        // constant right movement
+        rb.linearVelocity = new Vector2(speed, rb.linearVelocity.y);
+        
+        // better jump gravity
+        if (rb.linearVelocity.y < 0)
+            rb.gravityScale = baseGravity * fallGravityMultiplier;
+        else if (rb.linearVelocity.y > 0)
+            rb.gravityScale = baseGravity * riseGravityMultiplier;
+        else
+            rb.gravityScale = baseGravity;
+    }
+
+    void OnJump()
+    {
+        if (grounded)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpVelocity);
+    }
+    
+    void OnDrawGizmos()
+    {
+        if (groundCheck == null) return;
+        Gizmos.color = grounded ? Color.green : Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundRadius);
+    }
+
+
+}
