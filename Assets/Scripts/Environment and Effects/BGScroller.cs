867e49b14373cdae9366327f18b8753db9458a37
AppleXD1 <143732226+AppleXD1@users.noreply.github.com>
2026-02-17 20:09:27 -0500

    e

--- Assets/Scripts/Environment and Effects/BGScroller.cs
+++ Assets/Scripts/Environment and Effects/BGScroller.cs
@@ -5,17 +5,26 @@ public class BGScroller : MonoBehaviour
     public float scrollSpeed = 2f;
     public bool isScreen;
     public float width;
+    public Transform player;
+    public float lastPlayerLocation;
 
     void Start()
     {
         width = GetComponent<SpriteRenderer>().bounds.size.x;
+        lastPlayerLocation = player.position.x;
        
     }
 
     void Update()
     {
+        float playerMove = player.position.x - lastPlayerLocation;
+
+        if (playerMove > 0)
+        {
+            transform.Translate(Vector2.left * scrollSpeed * Time.deltaTime);
+        }
      
-        transform.Translate(Vector2.left * scrollSpeed * Time.deltaTime);
+        
 
 
 
--- Assets/Scripts/Environment and Effects/PlatformScroller.cs
+++ Assets/Scripts/Environment and Effects/PlatformScroller.cs
@@ -5,26 +5,35 @@ public class PlatformScroller : MonoBehaviour
     [SerializeField] private GameObject[] platform;
 
     public BGScroller BGScroller;
+    public Transform player;
     public float platformSpawnTime = 2f;
     public float timeUntilPlatform = 3f;
+    private float lastPlayerX;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        lastPlayerX = player.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        timeUntilPlatform -= Time.deltaTime;
-
-        if (timeUntilPlatform < 0)
+        float playerMove = player.position.x - lastPlayerX;
+        if (playerMove > 0)
         {
-            SpawnLoop();
-            timeUntilPlatform = 3;
+            timeUntilPlatform -= Time.deltaTime;
+
+            if (timeUntilPlatform < 0)
+            {
+                SpawnLoop();
+                timeUntilPlatform = 3;
+            }
+
         }
-        
+
+        lastPlayerX = player.position.x;
+
     }
 
     public void SpawnLoop()
