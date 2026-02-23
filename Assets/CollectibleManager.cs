afbe9953323f2b90ea4adb1702bceba2e3a2f988
Mattheww31 <49414872+Mattheww31@users.noreply.github.com>
2026-02-23 16:13:43 -0500

    -collectible

---
+++ Assets/CollectibleManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using TMPro;
+public class CollectibleManager : MonoBehaviour
+{
+    public static TMP_Text text;
+    public static float CollectibleAmount;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Collect(){
+         Destroy(gameObject);
+        CollectibleAmount+=1;
+    }
+    private void OnTriggerEnter2D(Collider2D col){
+       Collect();
+    }
+}
