b1a7dd174b2e8ad210e81822f6a1138a7e521833
Mattheww31 <49414872+Mattheww31@users.noreply.github.com>
2026-03-02 20:42:31 -0500

    -jump fix

--- Assets/Scripts/Enemy Functionality/Spawn.cs
+++ Assets/Scripts/Enemy Functionality/Spawn.cs
@@ -14,6 +14,7 @@ public class Spawn : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
+        ScoreManager manager = new ScoreManager();
         if(manager.CurrentScore>1000){
              if(obj=="Snake")
                 gameObject.name="Mosquito";
